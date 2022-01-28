953fc6fd8c722f75213fb09993f7c8ab8c93fd9a
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2022-01-28 07:56:34 -0700

    Building API (In Progress)
    Work in progress on creating Api.js and connecting it to App.js

--- restaurant-reservation-api/src/App.js
+++ restaurant-reservation-api/src/App.js
@@ -1,6 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+const api = require('./Api');
+
+// Restaurant endpoints
+App.post('/restaurants/', api.postRestaurant);
+App.put('/restaurants/:id', api.putRestaurant);
+App.get('/restaurants/:id', api.getRestaurant);
+App.get('/restaurants/', api.getAllRestaurants);
+App.delete('/restaurants/:id', api.deleteRestaurant);
+
+// Reservation endpoints
+//App.post('/reservations/post', api.postReservation);
+//App.put('/reservations/put', api.putReservation);
+//App.get('/reservations/:id', api.getReservation);
+//App.patch('/reservations/patch', api.patchReservation);
+//App.delete('/reservations/:id', api.deleteReservation);
+
+// Table endpoints
+//App.post('/tables/post', api.postTable);
+//App.put('/tables/put', api.putTable);
+//App.get('/tables/:id', api.getTable);
+//App.patch('/tables/patch', api.patchTable);
+//App.delete('/tables/:id', api.deleteTable);
+
 function App() {
   return (
     <div className="App">
