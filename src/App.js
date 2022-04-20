d49bad819628084411da6fee2a2dfd5c3dfc5b52
schroederjacob <60017931+schroederjacob@users.noreply.github.com>
2022-04-19 18:10:36 -0600

    Remove jwt
    Removed the jwt stuff.

--- src/App.js
+++ src/App.js
@@ -5,62 +5,34 @@ const bodyParser = require('body-parser');
 const api = require('./Api');
 const port = 3000;
 
-const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
-const jwks = require('jwks-rsa');
-
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, }));
-
-// Authorization middleware.
-var jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: 'https://dev-trs7ilhe.us.auth0.com/.well-known/jwks.json'
-    }),
-    audience: 'localhost:3000/',
-    issuer: 'https://dev-trs7ilhe.us.auth0.com/',
-    algorithms: ['RS256']
-});
-
-var options = { customScopeKey: 'permissions'};  // This is necessary to support the direct-user permissions
-const checkScopes = jwtAuthz([ 'read:restaurants' ]);
 
 app.listen(port, () => {
     console.log(`Restaurant app is running on port ${port}.`);
 });
 
 // Restaurant endpoints
-app.post('/restaurants/', jwtCheck, api.addRestaurant);
-app.put('/restaurants/:id', jwtCheck, api.updateRestaurant);
-app.get('/restaurants/:id', jwtCheck, api.getRestaurant);
-app.get('/restaurants/', jwtCheck, api.getAllRestaurants);
-app.patch('/restaurants/:id', jwtCheck, api.replaceRestaurant);
-app.delete('/restaurants/:id', jwtCheck, api.deleteRestaurant);
+app.post('/restaurants/', api.addRestaurant);
+app.put('/restaurants/:id', api.updateRestaurant);
+app.get('/restaurants/:id', api.getRestaurant);
+app.get('/restaurants/', api.getAllRestaurants);
+app.patch('/restaurants/:id', api.replaceRestaurant);
+app.delete('/restaurants/:id', api.deleteRestaurant);
 
 // Reservation endpoints
 app.post('/reservations/', api.addReservation);
-app.put('/reservations/:confirmation_number', api.updateReservation);
-app.get('/reservations/:confirmation_number', api.getReservation);
-app.get('reservations/', api.getAllReservations);
-app.patch('/reservations/:confirmation_number', api.replaceReservation);
-app.delete('/reservations/:confirmation_number', api.deleteReservation);
-
-// Table endpoints
-app.post('/tables/', api.addTable);
-app.put('/tables/:restaurant_id/:table_id', api.updateTable);
-app.get('/tables/:restaurant_id/:table_id', api.getTable);
-app.get('/tables/', api.getAllTables);
-app.patch('/tables/:restaurant_id/:table_id', api.replaceTable);
-app.delete('/tables/:restaurant_id/:table_id', api.deleteTable);
+app.put('/reservations/:restaurant_id/:confirmation_number', api.updateReservation);
+app.get('/reservations/:restaurant_id/:confirmation_number', api.getReservation);
+app.get('/reservations/', api.getAllReservations);
+app.patch('/reservations/:restaurant_id/:confirmation_number', api.replaceReservation);
+app.delete('/reservations/:restaurant_id/:confirmation_number', api.deleteReservation);
 
 // Employee endpoints
-app.post('/employees/', jwtCheck, api.addEmployee);
-app.put('/employees/:restaurant_id/:emp_username', jwtCheck, api.updateEmployee);
-app.get('/employees/:restaurant_id/:emp_username', jwtCheck, api.getEmployee);
-app.get('/employees/', jwtCheck, api.getAllEmployees);
-app.patch('/employees/:restaurant_id/:emp_username', jwtCheck, api.replaceEmployee);
-app.delete('/employees/:restaurant_id/:emp_username', jwtCheck, api.deleteEmployee);
+app.post('/employees/', api.addEmployee);
+app.put('/employees/:restaurant_id/:emp_username', api.updateEmployee);
+app.get('/employees/:restaurant_id/:emp_username', api.getEmployee);
+app.get('/employees/', api.getAllEmployees);
+app.patch('/employees/:restaurant_id/:emp_username', api.replaceEmployee);
+app.delete('/employees/:restaurant_id/:emp_username', api.deleteEmployee);
